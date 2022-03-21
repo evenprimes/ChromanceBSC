66e03e787a2fb9e4cb4b31a8a166f59737679de4
Chris Cioffi <chris@evenprimes.com>
2022-03-21 16:44:52 -0400

    Cleared out unused Chase code

--- src/main.cpp
+++ src/main.cpp
@@ -21,10 +21,10 @@ Blue - Blue
 #define FASTLED_INTERNAL
 
 // #include "SimpleChase.h"
-#include "RainbowChase.h"
+// #include "RainbowChase.h"
 #include "RainbowRipple.h"
 
-#define DATA_PIN1 D1
+#define DATA_PIN1 D1 // Oops! Probably should not have used D1 since now the onboard LED can't be turned off.
 #define DATA_PIN2 D2
 #define DATA_PIN3 D3
 #define DATA_PIN4 D4
@@ -39,20 +39,22 @@ Blue - Blue
 
 // int lengths[] = {154, 168, 84, 154}; // Strips are different lengths because I am a dumb
 uint8_t brightness = 255;
-uint8_t fade_factor = 20;
+// uint8_t fade_factor = 20;
 uint8_t blend_factor = 20;
 int pattern = 1;
 uint8_t sync = 0;
+unsigned long new_velocity = 0;
+uint8_t starting_hue = 0;
 
 CRGBArray<LEN0> leds0;
 CRGBArray<LEN1> leds1;
 CRGBArray<LEN2> leds2;
 CRGBArray<LEN3> leds3;
 
-RainbowChase stripred = RainbowChase(leds0, random8(), 17, LEN0, 50);
-RainbowChase stripblue = RainbowChase(leds1, random8(), 19, LEN1, 40);
-RainbowChase stripgreen = RainbowChase(leds2, random8(), 27, LEN2, 75);
-RainbowChase stripviolet = RainbowChase(leds3, random8(), 7, LEN3, 100);
+// RainbowChase stripred = RainbowChase(leds0, random8(), 17, LEN0, 50);
+// RainbowChase stripblue = RainbowChase(leds1, random8(), 19, LEN1, 40);
+// RainbowChase stripgreen = RainbowChase(leds2, random8(), 27, LEN2, 75);
+// RainbowChase stripviolet = RainbowChase(leds3, random8(), 7, LEN3, 100);
 
 RainbowRipple rr1 = RainbowRipple(leds0, LEN0, random8(), 5, 20);
 RainbowRipple rr2 = RainbowRipple(leds1, LEN1, random8(), 17, random16(VELOCITY_MIN, VELOCITY_MAX));
@@ -103,10 +105,10 @@ void loop()
 {
     if (pattern == 0)
     {
-        stripred.Draw(fade_factor);
-        stripblue.Draw(fade_factor);
-        stripgreen.Draw(fade_factor);
-        stripviolet.Draw(fade_factor);
+        // stripred.Draw(fade_factor);
+        // stripblue.Draw(fade_factor);
+        // stripgreen.Draw(fade_factor);
+        // stripviolet.Draw(fade_factor);
     }
     else
     {
@@ -118,22 +120,29 @@ void loop()
     FastLED.setBrightness(brightness);
     FastLED.show();
 
-    EVERY_N_SECONDS(10)
+    EVERY_N_SECONDS(15)
     {
         if (sync)
         {
-            unsigned long new_velocity = random16(VELOCITY_MIN, VELOCITY_MAX);
-            rr1.set_hue_and_progression(0, 2);
+            // Setting the starting hue to a random point around the color wheel.
+            // Starting at 0 was... boring.
+            new_velocity = random16(VELOCITY_MIN, VELOCITY_MAX);
+            starting_hue = random8();
+            rr1.set_hue_and_progression(starting_hue, 2);
             rr1.set_velocity(new_velocity);
-            rr2.set_hue_and_progression(0, 2);
+            rr2.set_hue_and_progression(starting_hue, 2);
             rr2.set_velocity(new_velocity);
-            rr3.set_hue_and_progression(0, 2);
+            rr3.set_hue_and_progression(starting_hue, 2);
             rr3.set_velocity(new_velocity);
-            rr4.set_hue_and_progression(0, 2);
+            rr4.set_hue_and_progression(starting_hue, 2);
             rr4.set_velocity(new_velocity);
         }
         else
         {
+            // These colors shouldn't sync, so we just start at random points
+            // around the color wheel. I didn't want the colors to jump TOO much
+            // as they change so I'm limiting hue progression to 1-7. I'm not
+            // positive that this is the correct range, but it looks good!
             rr1.set_hue_and_progression(random8(), random8(1, 7));
             rr1.set_velocity(random16(VELOCITY_MIN, VELOCITY_MAX));
             rr2.set_hue_and_progression(random8(), random8(1, 7));
